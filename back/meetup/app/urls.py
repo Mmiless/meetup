e4701f560662434dfa4b022bcba5f27a3f9044c0
Miles Kirkbride <miles.kirkbride@yahoo.com>
2024-12-27 14:46:19 -0800

    Backend json serialization fix

--- back/meetup/app/urls.py
+++ back/meetup/app/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('newevent/', create_event, name='create-event'),
     path('getevent/', get_event, name='get-event'),
-    path('login/', verify_login, name='verify-login')
+    path('login/', verify_login, name='verify-login'),
+    path('update/', update_user_times, name='update-user-times'),
 ]
--- back/meetup/app/views.py
+++ back/meetup/app/views.py
@@ -38,12 +38,25 @@ def verify_login(request):
     participants = event.participants
     if username in participants:
         if participants[username]['password'] == password:
-            return Response(participants['username'], status=status.HTTP_200_OK)
+            return Response(participants[username], status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
     participants[username] = {'password': password, 'times': {}}
-    event.participants = participants
+    event.participants = json.dumps(participants)
     event.save()
-    return Response(participants['username'], status=status.HTTP_200_OK)
+    return Response(participants[username], status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def update_user_times(request):
+    data = request.data
+    event_hash = data['hash']
+    username = data['username']
+    times = data['times']
+    event = Event.objects.get(hash=event_hash)
+    participants = event.participants
+    participants[username]['times'] = times
+    event.participants = json.dumps(participants)
+    event.save()
+    return Response(participants[username], status=status.HTTP_200_OK)
             
             
